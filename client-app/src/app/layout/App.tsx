0eabd3406b90904433248e849e0a2f817ed747a8
Przemysław Całka <przemekcalka1@outlook.com>
2020-08-13 20:23:14 +0200

    Creating activity list.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,32 +1,29 @@
-import React, {useState, useEffect} from 'react';
-import { Header, Icon, List } from 'semantic-ui-react'
-import axios from 'axios';
-import { IActivity } from '../models/activity';
-import { NavBar } from '../../features/nav/NavBar';
-
+import React, { useState, useEffect, Fragment } from "react";
+import { Container } from "semantic-ui-react";
+import axios from "axios";
+import { IActivity } from "../models/activity";
+import { NavBar } from "../../features/nav/NavBar";
+import { ActivityDashboard } from "../../features/activities/dashboard/ActivityDashboard";
 
 const App = () => {
-  const [activities,  setActivities] = useState<IActivity[]>([])
+  const [activities, setActivities] = useState<IActivity[]>([]);
 
-  useEffect(()=>{
-    axios.
-      get<IActivity[]>('http://localhost:5000/api/activities')
-      .then(response =>{
-       setActivities(response.data)  
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setActivities(response.data);
       });
-    }, []);
+  }, []);
 
-    return (
-      <div >
-         <NavBar></NavBar>
-         <List>
-         {activities.map((activities)=>(
-              <List.Item key={activities.id}>{activities.title}</List.Item>
-            ))}
-          </List>
-      </div>
-    );
-  
+  return (
+    <Fragment>
+      <NavBar/>
+      <Container style={{marginTop: '7em'}}>
+        <ActivityDashboard activities={activities}/>
+      </Container>
+    </Fragment>
+  );
 };
 
 export default App;
