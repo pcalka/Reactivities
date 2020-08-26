b74fc8b4ecc411ad8a9750e8b8e5873383c8bb85
Przemysław Całka <przemekcalka1@outlook.com>
2020-08-26 06:06:37 +0200

    Add to client app uuid package.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -26,7 +26,19 @@ const App = () => {
     setSelectedActivity(null);
     setEditMode(true);
   }
+
+  const handleCreateActivity = (activity : IActivity) => {
+    setActivities([...activities, activity])
+    setSelectedActivity(activity);
+    setEditMode(false);
+  }
   
+  const handleEditActivity = (activity : IActivity) => {
+    setActivities([...activities.filter(a => a.id !== activity.id), activity])
+    setSelectedActivity(activity);
+    setEditMode(false);
+  }
+
   useEffect(() => {
     axios
       .get<IActivity[]>("http://localhost:5000/api/activities")
@@ -46,6 +58,8 @@ const App = () => {
           editMode={editMode}
           setEditMode={setEditMode}
           setSelectedActivity={setSelectedActivity}
+          createActivity={handleCreateActivity}
+          editActivity={handleEditActivity}
         />
       </Container>
     </Fragment>
